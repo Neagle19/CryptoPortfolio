3f01189b25cd00ad61756022ce3dc89692c9064c
Shane <shane@genuine-standards.com>
2019-08-27 14:02:38 +0200

    Devise installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # For the Devise gem - when app is complete, comment in the following and add URL:
+  # config.action_mailer.default_url_options = { host: 'www.mywebsiteaddress.com' }
 end
